0d349241fbf4ff8a308d79959227052325c58a75
YuzeTT <ziv1024@163.com>
2023-08-09 13:16:37 +0800

    add: home ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 'use client'
 // import IndexHitokoto from '@/component/Index/IndexHitokoto';
-import {ActionButton, Breadcrumbs, Button, Cell, Column, Content, Heading, IllustratedMessage, Item, LabeledValue, Row, TableBody, TableHeader, TableView, Well} from '@adobe/react-spectrum'
+import {ActionButton, ActionMenu, Breadcrumbs, Button, Cell, Column, Content, Text, IllustratedMessage, Item, LabeledValue, ListView, Row, TableBody, TableHeader, TableView, Well, Heading, ActionGroup} from '@adobe/react-spectrum'
+import File from '@spectrum-icons/illustrations/File';
 import NotFound from '@spectrum-icons/illustrations/NotFound';
+import Edit from '@spectrum-icons/workflow/Edit';
+import Delete from '@spectrum-icons/workflow/Delete';
 // import { Suspense } from 'react';
+
+function renderEmptyState() {
+  return (
+    <IllustratedMessage>
+      <NotFound />
+      <Heading>No results</Heading>
+      <Content>No results found</Content>
+    </IllustratedMessage>
+  );
+}
 
 export default function Home() {
   return (
@@ -15,26 +28,35 @@ export default function Home() {
         <Heading>No results</Heading>
         <Content>Try another search</Content>
       </IllustratedMessage> */}
-      <TableView
-        aria-label="机器列表"
+      <div className='flex items-center justify-between pb-4'>
+        <h2 className='text-xl font-bold'>连接配置</h2>
+        <Button variant='accent'>新建</Button>
+      </div>
+      <ListView
+        selectionMode="none"
+        aria-label="Static ListView items example"
+        onAction={(key) => alert(`Triggering action on item ${key}`)}
+        renderEmptyState={renderEmptyState}
+        height={400}
       >
-        <TableHeader>
-          <Column>名称</Column>
-          <Column>类型</Column>
-          <Column>类型</Column>
-          <Column align="center">控制</Column>
-        </TableHeader>
-        <TableBody>
-          <Row>
-            <Cell>Games</Cell>
-            <Cell>File folder</Cell>
-            <Cell>6/7/2020</Cell>
-            <Cell>
-              <Button variant="secondary" style="fill">进入</Button>
-            </Cell>
-          </Row>
-        </TableBody>
-      </TableView>
+        {/* <Item>
+          <Text>连接配置</Text>
+          <ActionButton>新建</ActionButton>
+        </Item> */}
+        <Item key="1" textValue="Utilities">
+          <File />
+          <Text>DESK</Text>
+          <Text slot="description">192.168.1.8</Text>
+          {/* <ActionButton>
+            <ChevronRight/>
+          </ActionButton> */}
+          <ActionMenu>
+            <Item key="edit" textValue="Edit">编辑</Item>
+            <Item key="delete" textValue="Delete">删除</Item>
+          </ActionMenu>
+        </Item>
+      </ListView>
+      
       {/* <Well>
         <h2 className='font-bold text-lg mb-2'>测试机</h2>
         <LabeledValue label="IP" value="192.168.0.0" />
